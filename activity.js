7720497f747c922eac443d2aeb102027c9776239
MassimoGiorgio <64086350+xzmassimo21@users.noreply.github.com>
2023-07-25 10:49:26 +0200

    Create activity.js

---
+++ activity.js
@@ -0,0 +1,13 @@
+var connection = new postmonger.Session();
+
+//Startup sequence
+connection.trigger('ready');
+
+connection.on('initActivity', function(data) {
+  document.getElementById('configuration').value = JSON.stringify(data, null,2);
+});
+//Save sequence
+connection.on('clickedNext', function() {
+var configuration = JSON.parse(document.getElementById('configuration').value);
+  connection.trigger('updateActivity',configuration);  
+});
